8d3021420e11fbadec1b75fed50e0ca9d6535086
VuVanTu <vutu201130@gmail.com>
2017-10-25 14:55:39 +0700

    Fixed optimization log error & overflow run time warning

--- model.py
+++ model.py
@@ -15,7 +15,12 @@ class model():
         print 'labels', self.labels
         print 'lambda.shape',self.lmbda.shape
         self.is_trained = False
-        
+
+    def softmax(self,x):
+        """Compute softmax values for each sets of scores in x."""
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
+
                             
     def compute_contidional_prob(self, label_idx, doc, lmbda):
         _p = 0.0
@@ -41,11 +46,13 @@ class model():
     
     def compute_log_li_grad(self, lmbda):
         print 'Computing log li and grad'
-        log_li = -1.0/200 * np.sum(lmbda**2)
-        train_docs = self.data.train
-        grad = -1.0/100 * lmbda
+#         log_li = -1.0/200 * np.sum(lmbda**2)
+#         grad = -1.0/100 * lmbda
         
-        for doc in train_docs:
+        log_li = 0.0
+        grad = np.zeros(lmbda.shape)
+
+        for doc in self.data.train:
             ########### compute log_li #############
             doc_log_li = 0.0
             ep_1 = self.compute_sum_features(doc, doc.human_label, lmbda)
@@ -60,20 +67,22 @@ class model():
             ###### Update feature_idx with word_id in docs ##########
             word_ids = doc.cp_ids_counts.keys()
             # update feature idx with d.humanlabel
-            temp = np.zeros(self.label_count)
             for word_id in word_ids:
                 feature_idx = doc.human_label * self.V_count + word_id
                 grad[feature_idx] += 1.0 * doc.cp_ids_counts[word_id] / doc.length
-                
-                for label_idx in self.labels:
-                    temp[label_idx] += 1.0 * lmbda[label_idx * self.V_count + word_id] * doc.cp_ids_counts[word_id] / doc.length
             
-            sum_exp_temp = np.sum(np.exp(temp))
+            temp = np.zeros(self.label_count)                
+            for label_idx in self.labels:
+                temp[label_idx] = self.compute_sum_features(doc, label_idx, lmbda)
+        
+            temp = self.softmax(temp)
+            
+#             sum_exp_temp = np.sum(np.exp(temp))
             
             for word_id in word_ids:
                 for label_idx in self.labels:
                     feature_idx = label_idx * self.V_count + word_id
-                    grad[feature_idx] -= temp[label_idx] / sum_exp_temp
+                    grad[feature_idx] -= 1.0 * doc.cp_ids_counts[word_id] / doc.length * temp[label_idx]
             
         return -log_li, np.negative(grad)
 
@@ -81,8 +90,7 @@ class model():
     
         
     def train(self):
-        self.lmbda, log_li, dic = fmin_l_bfgs_b(self.compute_log_li_grad, self.lmbda, fprime = None, \
-                                                iprint = 99, pgtol=1e-10, factr=1e-30)
+        self.lmbda, log_li, dic = fmin_l_bfgs_b(self.compute_log_li_grad, self.lmbda, iprint = 99)
         self.is_trained = True
         print 'trained. lambda = ', self.lmbda
     
@@ -99,6 +107,7 @@ class model():
     
     def validate(self):
         num_doc_tests = len(self.data.test)
+        print 'number of doc test', num_doc_tests
         pre = 0
         for doc in self.data.test:
             if doc.human_label == doc.model_label:
