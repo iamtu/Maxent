296e08fd632a9fcbfa15debecc907d8053e75292
VuVanTu <vutu201130@gmail.com>
2017-10-25 10:43:53 +0700

    For each doc, update grad_idx in which contains word_id

--- model.py
+++ model.py
@@ -7,87 +7,92 @@ class model():
         self.label_count = len(self.data.labels)
         self.labels = self.data.labels
         self.V_count = len(self.data.cp_map)
-        self.num_feafures = self.label_count * self.V_count
+        self.V = self.data.cp_map.values()
         
-        self.lmbda = np.zeros(self.num_feafures)
+        self.lmbda = np.zeros( self.label_count* self.V_count)
         print 'number of context predicates: ', self.V_count
         print 'labels', self.labels
+        print 'lambda.shape',self.lmbda.shape
         self.is_trained = False
         
-        
-    def map_feature_idx_2_cp_idx_label(self, feature_idx):
-        label_idx = feature_idx / self.V_count
-        cp_idx = feature_idx % self.V_count
-        return (label_idx, cp_idx)
-        
-    def compute_feature(self, feature_idx, doc, label):
+            
+    def compute_feature(self, label_idx, word_id, doc, label):
         feature_value = 0.0
-        (label_idx, cp_idx) = self.map_feature_idx_2_cp_idx_label(feature_idx)
-        if (label_idx == label) and (cp_idx in doc.cp_ids_counts):
-            feature_value = 1.0 * doc.cp_ids_counts[cp_idx] / doc.length
+        if (label_idx == label) and (word_id in doc.cp_ids_counts):
+            feature_value = 1.0 * doc.cp_ids_counts[word_id] / doc.length
         else:
-            feature_value = 0.0
-            
-#         print '\n---compute feature for doc - label'
-#         print 'word,label', cp_idx, label_idx 
-#         print 'doc', doc
-#         print 'label', label
-#         print 'feature value', feature_value
-        
+            feature_value = 0.0        
         return feature_value
-        
-    def compute_log_li(self, lmbda):
-        log_li = 0.0
-        train_docs = self.data.train
-        
-        for doc in train_docs:
-            doc_log_li = 0.0
-            ep_1 = 0.0
-            for i in xrange(len(lmbda)):
-                ep_1 += 1.0 * lmbda[i] * self.compute_feature(i, doc, doc.human_label)
-            
-            ep_2 = 0.0           
-            for label_idx in self.labels:
-                temp = 0.0
-                for i in xrange(len(lmbda)):
-                    temp += 1.0 * lmbda[i] * self.compute_feature(i, doc, label_idx)
-                ep_2 += np.exp(temp)    
-            
-            doc_log_li = ep_1 - np.log(ep_2)
-            log_li += doc_log_li
-        return -log_li
-        
+                
     def compute_contidional_prob(self, label_idx, doc, lmbda):
         p = 0.0
-        _numerator = 0.0
-        for i in xrange(len(lmbda)):
-            _numerator += lmbda[i] * self.compute_feature(i, doc, label_idx)
+        _numerator = self.compute_sum_features(doc, label_idx, lmbda)
         _numerator = np.exp(_numerator)
         
         _demoninator = 0.0
         for label_ in self.labels:
-            temp = 0.0
-            for i in xrange(len(lmbda)):
-                temp += lmbda[i] * self.compute_feature(i, doc, label_)
+            temp = self.compute_sum_features(doc, label_, lmbda)
             _demoninator += np.exp(temp)
         p = _numerator / _demoninator
         return p
-    def compute_grad(self, lmbda):
-        grad = np.zeros(len(lmbda))
-        for feature_idx in range(len(grad)):
-            grad[feature_idx] = 0.0
-            for doc in self.data.train:
-                ep_1 = self.compute_feature(feature_idx, doc, doc.human_label)
-                ep_2 = 0.0
-                for label_idx in self.labels:
-                    ep_2 += self.compute_feature(feature_idx, doc, label_idx) * self.compute_contidional_prob(label_idx, doc, lmbda)
-                grad[feature_idx] += ep_1 - ep_2
-        return np.negative(grad)
+
+    def compute_sum_features(self, doc, label_idx, lmbda):
+        '''
+        compute sum_i lambda[i]* f_i (doc, label_idx)
+        '''
+        sum = 0.0
+        word_ids = doc.cp_ids_counts.keys()
+        for word_id in word_ids:
+            sum += 1.0 * lmbda[label_idx * self.V_count + word_id] * doc.cp_ids_counts[word_id] / doc.length
+        return sum
+    
+    def compute_log_li_grad(self, lmbda):
+        print 'Computing log li and grad'
+        log_li = -1.0/200 * np.sum(lmbda**2)
+        train_docs = self.data.train
+        grad = -1.0/100 * lmbda
         
+        for doc in train_docs:
+            ########### compute log_li #############
+            doc_log_li = 0.0
+            ep_1 = self.compute_sum_features(doc, doc.human_label, lmbda)
+            
+            ep_2 = 0.0           
+            for label_idx in self.labels:
+                temp = self.compute_sum_features(doc, label_idx, lmbda)                
+                ep_2 += np.exp(temp)    
+            
+            doc_log_li = ep_1 - np.log(ep_2)
+            log_li += doc_log_li
+            
+            ###### Update feature_idx with word_id in docs ##########
+            word_ids = doc.cp_ids_counts.keys()
+            # update feature idx with d.humanlabel
+            temp = np.zeros(self.label_count)
+            for word_id in word_ids:
+                feature_idx = doc.human_label * self.V_count + word_id
+                grad[feature_idx] += 1.0 * doc.cp_ids_counts[word_id] / doc.length
+                
+                for label_idx in self.labels:
+                    temp[label_idx] += 1.0 * lmbda[label_idx * self.V_count + word_id] * doc.cp_ids_counts[word_id] / doc.length
+            
+            sum_exp_temp = np.sum(np.exp(temp))
+            
+            for word_id in word_ids:
+                for label_idx in self.labels:
+                    feature_idx = label_idx * self.V_count + word_id
+                    grad[feature_idx] -= temp[label_idx] / sum_exp_temp
+            
+        return -log_li, np.negative(grad)
+
+    #########################################################
+    
         
     def train(self):
-        self.lmbda, log_li, dic = fmin_l_bfgs_b(self.compute_log_li, self.lmbda, self.compute_grad, iprint = 99)
+        self.lmbda, log_li, dic = fmin_l_bfgs_b(self.compute_log_li_grad, self.lmbda, fprime = None, \
+                                                iprint = 99, pgtol=1e-10, factr=1e-30)
         self.is_trained = True
+        print 'trained. lambda = ', self.lmbda
     
     def inference(self):
         if not self.is_trained:
